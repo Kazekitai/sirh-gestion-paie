b5d215bc57652ad0270d3fe7b1f9a2c9ccbbddb4
Sandra LE THIEC <lethiec.sandra@gmail.com>
2017-12-20 09:08:57 +0100

    TP05 - Spring JPA DATA - test Entite Avantage complet

--- src/main/java/dev/paie/repository/AvantageRepository.java
+++ src/main/java/dev/paie/repository/AvantageRepository.java
@@ -1,17 +1,22 @@
 package dev.paie.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import dev.paie.entite.Avantage;
 
 /**
  * Interface repository pour les avantages
+ * 
  * @author Sandra Le Thiec
  *
  */
 @Repository
+@Transactional
 public interface AvantageRepository extends JpaRepository<Avantage, Integer> {
-	
-	Avantage findOne(String code);
+	List<Avantage> findByCode(String code);
 
 }
--- src/test/java/dev/paie/repository/AvantageRepositoryTest.java
+++ src/test/java/dev/paie/repository/AvantageRepositoryTest.java
@@ -1,5 +1,6 @@
 package dev.paie.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
 import java.math.BigDecimal;
@@ -14,11 +15,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import dev.paie.config.ServicesConfig;
 import dev.paie.entite.Avantage;
+import dev.paie.util.PaieUtils;
 
 //Sélection des classes de configuration Spring à utiliser lors du test
-@ContextConfiguration(classes = { ServicesConfig.class})
-//Configuration JUnit pour que Spring prenne la main sur le cycle de vie du
-//test
+@ContextConfiguration(classes = { ServicesConfig.class })
+// Configuration JUnit pour que Spring prenne la main sur le cycle de vie du
+// test
 @RunWith(SpringRunner.class)
 public class AvantageRepositoryTest {
 	private final Logger LOG = LoggerFactory.getLogger("logger1");
@@ -26,21 +28,44 @@ public class AvantageRepositoryTest {
 	@Autowired
 	private AvantageRepository avantageRepository;
 
+	@Autowired
+	private PaieUtils paieUtils;
+
 	@Test
 	public void test_sauvegarder_lister_mettre_a_jour() {
 	// TODO sauvegarder un nouvel avantage
 		Avantage avantage = new Avantage();
 		avantage.setCode("CF");
-		avantage.setMontant(new BigDecimal("150"));
+		avantage.setMontant(new BigDecimal("150.20"));
 		avantage.setNom("repas");
-		avantageRepository.save(avantage);
+		Avantage newAvantage = avantageRepository.save(avantage);
 		
 	// TODO vérifier qu'il est possible de récupérer le nouvel avantage via la méthode findOne
-		Avantage newAvantage = avantageRepository.findOne(avantage.getCode());
-		LOG.info(newAvantage.toString());
+		Avantage newAvantageRecup = avantageRepository.findOne(newAvantage.getId());
+		assertThat(newAvantageRecup.getCode().equals("CF"));
+		assertThat(newAvantageRecup.getNom().equals("repas"));
+		assertThat(paieUtils.formaterBigDecimal(newAvantageRecup.getMontant()).equals(paieUtils.formaterBigDecimal(new BigDecimal("150.20"))));
+		LOG.info("\nAvantage ajouté: " + newAvantageRecup.toString());
 		
 	// TODO modifier un avantage
+		newAvantageRecup.setCode("CFModif");
+		newAvantageRecup.setMontant(new BigDecimal("500.50"));
+		newAvantageRecup.setNom("déjeuner");
+		Avantage avantageModif = avantageRepository.save(newAvantageRecup);
+		
 	// TODO vérifier que les modifications sont bien prises en compte via la méthode findOne
+		Avantage avantageModifRecup = avantageRepository.findOne(avantageModif.getId());
+		assertThat(avantageModifRecup.getCode().equals("CFModif"));
+		assertThat(avantageModifRecup.getNom().equals("déjeuner"));
+		assertThat(paieUtils.formaterBigDecimal(avantageModifRecup.getMontant()).equals(paieUtils.formaterBigDecimal(new BigDecimal("500.50"))));
+		LOG.info("\nAvantage modifié: " + avantageModifRecup.toString());
+		
+	// TODO vérifier que les modifications sont bien prises en compte via la méthode findByCode
+			Avantage avantageModifRecupByCode = avantageRepository.findByCode(avantageModifRecup.getCode()).get(0);
+			assertThat(avantageModifRecupByCode.getCode().equals("CFModif"));
+			assertThat(avantageModifRecupByCode.getNom().equals("déjeuner"));
+			assertThat(paieUtils.formaterBigDecimal(avantageModifRecupByCode.getMontant()).equals(paieUtils.formaterBigDecimal(new BigDecimal("500.50"))));
+			LOG.info("\nAvantage modifié (recherche par code): " + avantageModifRecupByCode.toString());
 	}
 
 }
