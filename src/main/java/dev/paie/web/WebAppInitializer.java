4b375ce74c644de0b212f2771a3bee0baf41370c
Sandra LE THIEC <lethiec.sandra@gmail.com>
2017-12-20 17:44:07 +0100

    TP 06 - Spring Web MVC en cours

--- src/main/java/dev/paie/web/WebAppInitializer.java
+++ src/main/java/dev/paie/web/WebAppInitializer.java
@@ -3,16 +3,19 @@ package dev.paie.web;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
 import dev.paie.config.WebAppConfig;
+import dev.paie.service.InitialiserDonneesServiceDev;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 
+	@Autowired
+	private InitialiserDonneesServiceDev initialiserDonneesServiceDev;
+	
 	@Override
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		// Initialisation du contexte Spring
@@ -43,6 +46,8 @@ public class WebAppInitializer implements WebApplicationInitializer {
 		 * </listener>
 		 */
 		servletContext.addListener(new ContextLoaderListener(webContext));
+		
+//		initialiserDonneesServiceDev.initialiser();
 
 	}
 
